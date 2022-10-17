1be66526f972fe5d1ccad4bdb0a5f6fc1820aa81
ianMuchesia <ianmuchesia@yahoo.com>
2022-10-17 16:14:59 +0300

    stage-3-create-react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,4 +12,23 @@ function App() {
         </a>
       </header>
       <main>
-        <h1>Featured Products</
+        <h1>Featured Products</h1>
+        <div className="flex flex-wrap">
+          {data.products.map((product) => (
+            <div key={product.slug} className="border border-black">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="md:h-[480px] md:w-64  md:m- h-48 w-24"
+              />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: "Women Dresses Crewneck Button Sleeveless Mini Dress",
+      slug: "Womens-Dresses-Crewneck-Button-Sleeveless-Mini-Dress",
+      category: "Dresses",
+      price: 1200,
+      image:
+        "/images/Women's Dresses Crewneck Button Sleeveless Mini Dress.jpg",
+      countInStock: 10,
+      brand: "Winnys",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "a beautiful Women Dress Crewneck Button Sleeveless Mini Dress",
+    },
+    {
+      name: "Women's Luxury Leather Alligator Handbag - Sienna",
+      slug: "Women's-Luxury-Leather-Alligator-Handbag-Sienna",
+      category: "Dresses",
+      price: 1200,
+      image: "/images/Women's Luxury Leather Alligator Handbag - Sienna.jpg",
+      countInStock: 10,
+      brand: "Winnys",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "a beautiful Women's Luxury Leather Alligator Handbag - Sienna",
+    },
+    {
+      name: "black Heels",
+      slug: "black-heels",
+      category: "shoes",
+      price: 1200,
+      image: "/images/blackHeels.jpg",
+      countInStock: 10,
+      brand: "Winnys",
+      rating: 4.5,
+      numReviews: 10,
+      description: "a beautiful black dress",
+    },
+    {
+      name: "Sleeve & short dress",
+      slug: "black-sleeve-dress",
+      category: "Dresses",
+      price: 1200,
+      image: "/images/blackSleeveShortDress.jpg",
+      countInStock: 10,
+      brand: "Winnys",
+      rating: 4.5,
+      numReviews: 10,
+      description: "a beautiful black dress",
+    },
+  ],
+};
+export default data;
