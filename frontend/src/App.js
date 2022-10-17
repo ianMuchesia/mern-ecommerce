d7f45a9a1fe793b106d7fb92a2de559f37c89922
ianMuchesia <ianmuchesia@yahoo.com>
2022-10-17 11:12:07 +0300

    stage-3-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import data from "./data";
+
+function App() {
+  return (
+    <div className="App">
+      <header
+        className="bg-[#403493] p-4
+      "
+      >
+        <a href="/" className="text-[#F9F7F3] font-bold no-underline">
+          WCC
+        </a>
+      </header>
+      <main>
+        <h1>Featured Products</
